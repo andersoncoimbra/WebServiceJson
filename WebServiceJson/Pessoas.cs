6f6c698d5132c084d90ea0637b64afbc15e28d80
Anderson Coimbra <andersonmegax@hotmail.com>
2015-11-19 04:01:48 -0200

    WebServiceJson
    Comite de criaçao de Repositorio

---
+++ WebServiceJson/Pessoas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceJson
+{
+    class Pessoas
+    {
+        
+        private string nome;
+        private string idade;
+        private string cidade;
+               
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+
+        }
+
+        public string Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        public string Cidade
+        {
+            get { return cidade; }
+            set { cidade = value; }
+        }
+        
+
+       
+        
+       
+
+    }
+}
---
+++ WebServiceJson/Program.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServiceJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            carragadorDadosRemotoAssicrono();
+                      
+            Console.ReadKey();
+
+
+            
+
+        }
+
+        public static async Task carragadorDadosRemotoAssicrono()
+        {
+            var client = new HttpClient();
+            client.MaxResponseContentBufferSize = 1024 * 1024;
+            var response = await client.GetAsync(new Uri("http://10.0.0.103/webservice/json.txt"));
+            var result = await response.Content.ReadAsStringAsync();
+            if (result.Length < 2)
+            {
+                Console.WriteLine("Nenhum cadastro");
+                Console.WriteLine(result.Length);
+            }
+            
+            Debug.WriteLine("------     "+result);
+            List<Pessoas> pessoas = JsonConvert.DeserializeObject<List<Pessoas>>(result);
+            Console.WriteLine("Total de registros "+pessoas.Count);
+            Console.WriteLine("~~~~~~~~~~~~~~");
+            int n = 0;
+            while (n < pessoas.Count)
+            {
+                Console.WriteLine(pessoas[n].Nome);
+                Console.WriteLine(pessoas[n].Idade);
+                Console.WriteLine(pessoas[n].Cidade);
+                Console.WriteLine("~~~~~~~~~~~~~~");
+                n++;
+            }
+            string type;
+            while (Console.ReadKey().ToString() != "i") {
+                type = Console.ReadKey().ToString();
+            }
+
+           
+        }
+
+       
+
+        public static string toUTF8(string response)
+        {
+            string junk = response;  // Unicode
+
+            // Turn string back to bytes using the original, incorrect encoding.
+            byte[] bytes = Encoding.GetEncoding(1252).GetBytes(junk);
+
+            // Use the correct encoding this time to convert back to a string.
+            string good = Encoding.ASCII.GetString(bytes);
+            //Console.WriteLine(good);
+            return good.ToString();
+        }
+
+
+        
+    }
+}
